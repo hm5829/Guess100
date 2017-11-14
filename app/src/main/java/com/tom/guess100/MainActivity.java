5d642840c3ccbb965f523e3fce09102a8f310b62
hm5829 <hm5829@yahoo.com.tw>
2017-11-14 10:04:29 +0800

    findViews

--- app/src/main/java/com/tom/guess100/MainActivity.java
+++ app/src/main/java/com/tom/guess100/MainActivity.java
@@ -2,12 +2,27 @@ package com.tom.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView secret;
+    private TextView info;
+    private EditText number;
+    private Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+    private void findViews(){
+        secret = (TextView) findViewById(R.id.secret);
+        info = (TextView) findViewById(R.id.info);
+        number = (EditText) findViewById(R.id.number);
+        send = (Button) findViewById(R.id.send);
     }
 }
